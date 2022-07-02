4fa36ca2ee93cf1552d04ed0798b1b936b99188b
David2508 <deividrocan2508@gmail.com>
2022-07-01 21:48:58 -0500

    add cast

--- 01Fundamentos/01Fundamentos.js
+++ 01Fundamentos/01Fundamentos.js
@@ -2,5 +2,11 @@
 // Tipos de datos, para ello se utiliza la palabra reservada "typeof"
 let hola= "Hola mundo";
 let indefinido
+let numero = 10;
+let booleano = true;
+let arreglo = [1,2,3,4,5];
 console.log(indefinido);
 console.log(typeof(hola));
+console.log(typeof(numero));
+console.log(typeof(booleano));
+console.log(typeof(arreglo));
---
+++ 01Fundamentos/02CastitngdeDatos.js
@@ -0,0 +1,24 @@
+// Conversion de datos
+/***
+ * 1. a String => String(), toString()
+ * 2. a Number => Number(), parseInt(), o si es en Decimal parseFloat()
+ * 3. a Boolean => Boolean(), parseBoolean()
+ * 4. a Array => Array(), parseArray()
+ * 5. a Object => Object(), parseObject()
+ * 6. a Function => Function(), parseFunction()
+ * 7. a Date => Date(), parseDate()
+ * 8. a RegExp => RegExp(), parseRegExp()
+ * 9. a Error => Error(), parseError()
+ * 10. a Null => Null(), parseNull()
+ * 11. a Undefined => Undefined(), parseUndefined()
+ * 12. a Symbol => Symbol(), parseSymbol()
+ */
+
+// De entero a string
+let dato = 10
+dato= dato.toString();
+console.log(typeof(dato));
+
+let dato2 = '45.5'
+dato2= parseInt(dato2);
+console.log(typeof(dato2));
